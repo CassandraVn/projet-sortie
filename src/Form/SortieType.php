db6229275d23e45c70dc243dbe46c15a0086d3c7
juliette3012 <juliette.donat03@hotmail.fr>
2022-12-06 12:25:45 +0100

    form New sortie + themeForm

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -2,27 +2,41 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
+use App\Entity\Lieu;
 use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use function PHPUnit\Framework\logicalAnd;
 
 class SortieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('dateHeureDebut')
-            ->add('duree')
-            ->add('dateLimiteInscription')
-            ->add('nbInscriptionMax')
-            ->add('infosSortie')
-            ->add('etat')
-            ->add('lieu')
-            ->add('campus')
-            ->add('Organisateur')
-            ->add('Participant')
+            ->add('nom', TextType::class, ['label'=>"Nom sortie"])
+            ->add('dateHeureDebut', DateTimeType::class, ['label'=>"Date de sortie"])
+            ->add('duree', IntegerType::class, ['label'=>"Duree"])
+            ->add('dateLimiteInscription' , DateTimeType::class, ['label'=>"Date limite d'inscription"])
+            ->add('nbInscriptionMax', IntegerType::class, ['label'=>"Nombre maximum de participants"])
+            ->add('infosSortie', TextareaType::class, ['label' => "Description"])
+            ->add('lieu' , EntityType::class, [
+                'label' => 'Lieu',
+                'class' => Lieu::class,
+                'choice_label' => 'nom'
+              ])
+            ->add('campus', EntityType::class, [
+                'label' => 'Campus',
+                'class' => Campus::class,
+                'choice_label' => 'nom'
+            ])
+
         ;
     }
 
