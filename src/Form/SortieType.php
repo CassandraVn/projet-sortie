93168149c8be3121c5221b30d9b83b1d2049c757
juliette3012 <juliette.donat03@hotmail.fr>
2022-12-06 15:04:36 +0100

    campus defaut form

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Campus;
 use App\Entity\Lieu;
 use App\Entity\Sortie;
+use App\Repository\CampusRepository;
+use App\Repository\UtilisateurRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
@@ -13,10 +15,19 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Security;
 use function PHPUnit\Framework\logicalAnd;
 
 class SortieType extends AbstractType
 {
+
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security=$security;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -30,13 +41,13 @@ class SortieType extends AbstractType
                 'label' => 'Lieu',
                 'class' => Lieu::class,
                 'choice_label' => 'nom'
-              ])
+             ])
             ->add('campus', EntityType::class, [
-                'label' => 'Campus',
                 'class' => Campus::class,
-                'choice_label' => 'nom'
-            ])
-
+                'label' => 'Campus',
+                'choice_label' => 'nom',
+                'data' => $this->security->getUser()->getCampus()
+                ])
         ;
     }
 
