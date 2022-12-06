d090c1f7d600330d8f821f73b5d74450bb4417be
CassandraVn <cassandra.vaslon2021@campus-eni.fr>
2022-12-06 15:03:45 +0100

    form user en cours

--- src/Form/UtilisateurType.php
+++ src/Form/UtilisateurType.php
@@ -2,8 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Entity\Utilisateur;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,17 +18,18 @@ class UtilisateurType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pseudo')
-            ->add('roles')
-            ->add('password')
-            ->add('nom')
-            ->add('prenom')
-            ->add('telephone')
-            ->add('actif')
-            ->add('photo')
-            ->add('mail')
-            ->add('SortiesParticipant')
-            ->add('campus')
+            ->add('pseudo', TextType::class, ['label'=>'Pseudo'])
+            ->add('password', PasswordType::class, ['label'=>'Mot de passe', 'required'=>false])
+            ->add('confirm', PasswordType::class, ['label'=>'Confirmation', 'required'=>false, 'mapped'=>false])
+            ->add('nom', TextType::class, ['label'=>'Nom'])
+            ->add('prenom', TextType::class, ['label'=>'Prénom'])
+            ->add('telephone', TelType::class, ['label'=>'Téléphone'])
+            ->add('mail', EmailType::class, ['label'=>'Email'])
+            ->add('campus', EntityType::class, [
+                'label'=>'Campus',
+                'class' => Campus::class,
+                'choice_label' => 'nom'
+            ])
         ;
     }
 
