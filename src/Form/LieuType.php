c63cf10ce3d70ca9a8d595c679333b61f20d00fe
juliette3012 <juliette.donat03@hotmail.fr>
2022-12-06 16:37:17 +0100

    form moche mais fonctionnelle

---
+++ src/Form/LieuType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lieu;
+use App\Entity\Ville;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LieuType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, ['label'=>"Nom"])
+            ->add('rue', TextType::class, ['label'=>"Rue"])
+            ->add('latitude', TextType::class, ['label'=>"Lat"])
+            ->add('longitude' , TextType::class, ['label'=>"long"])
+            ->add('ville', EntityType::class, [
+                'label'=>'Ville',
+                'class' => Ville::class,
+                'choice_label' => 'nom'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Lieu::class,
+        ]);
+    }
+}
