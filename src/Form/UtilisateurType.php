2034c60002b079ec531011ac2114734b757f047a
CassandraVn <cassandra.vaslon2021@campus-eni.fr>
2022-12-07 15:13:51 +0100

    photo user ok

--- src/Form/UtilisateurType.php
+++ src/Form/UtilisateurType.php
@@ -7,12 +7,14 @@ use App\Entity\Utilisateur;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class UtilisateurType extends AbstractType
 {
@@ -31,6 +33,13 @@ class UtilisateurType extends AbstractType
                 'class' => Campus::class,
                 'choice_label' => 'nom'
             ])
+            ->add('photo', FileType::class, ['label'=>'Ma photo', 'required'=>false, 'mapped'=> false, 'constraints'=>[
+                                new File([
+                                    'mimeTypes' => ['image/jpeg', 'image/png'],
+                                    'mimeTypesMessage' => 'Veuillez choisir une photo en format JPEG ou PNG.'
+                                ])
+
+                            ]])
             ->add('ajout', SubmitType::class, ['label'=>'Modifier'])
         ;
     }
