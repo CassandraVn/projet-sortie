7f736fe1b528a37b882767a1cba738e7af9f0519
A-Lorin <adrlien.lorin2021@campus-eni.fr>
2022-12-08 09:33:27 +0100

    remove comments

--- public/js/sortie/afficherSortie.js
+++ public/js/sortie/afficherSortie.js
@@ -1,10 +1,3 @@
-/*$('document').ready(function(){
-
-    document.querySelector('#dateDepuis').min = new Date().toJSON().slice(0,10);
-    document.querySelector('#dateUntil').min = new Date().toJSON().slice(0,10);
-});*/
-
-
 function checkDate(type)
 {
     var dateDebut,dateFin;
