46ba896be70c7ecc506f2101f7bca8c172271e1c
A-Lorin <adrlien.lorin2021@campus-eni.fr>
2022-12-13 12:28:27 +0100

    edit connexion impossible actif false

--- src/Security/UtilisateurAuthentificateurAuthenticator.php
+++ src/Security/UtilisateurAuthentificateurAuthenticator.php
@@ -2,11 +2,14 @@
 
 namespace App\Security;
 
+use App\Repository\UtilisateurRepository;
+use mysql_xdevapi\Exception;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
@@ -20,9 +23,11 @@ class UtilisateurAuthentificateurAuthenticator extends AbstractLoginFormAuthenti
     use TargetPathTrait;
 
     public const LOGIN_ROUTE = 'app_login';
+    private UtilisateurRepository $utilisateurRepository;
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    public function __construct(private UrlGeneratorInterface $urlGenerator, UtilisateurRepository $utilisateurRepository)
     {
+        $this->utilisateurRepository =  $utilisateurRepository;
     }
 
     public function authenticate(Request $request): Passport
@@ -30,6 +35,13 @@ class UtilisateurAuthentificateurAuthenticator extends AbstractLoginFormAuthenti
         $pseudo = $request->request->get('pseudo', '');
 
         $request->getSession()->set(Security::LAST_USERNAME, $pseudo);
+
+        $user = $this->utilisateurRepository->findOneBy(['pseudo'=>$pseudo]);
+        if( !$user->isActif() )
+        {
+            throw new AuthenticationException('Utilisateur inactif');
+        }
+
 
         return new Passport(
             new UserBadge($pseudo),
@@ -46,7 +58,7 @@ class UtilisateurAuthentificateurAuthenticator extends AbstractLoginFormAuthenti
             return new RedirectResponse($targetPath);
         }
 
-         return new RedirectResponse($this->urlGenerator->generate('app_utilisateur_index'));
+         return new RedirectResponse($this->urlGenerator->generate('app_sortie_index'));
     }
 
     protected function getLoginUrl(Request $request): string
